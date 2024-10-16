0e23a8bb65999fbb0e7b973b8c7c913bf561897e
alex <alexandre@garaia.fr>
2024-10-16 17:08:25 +0200

    Custom logos

--- main.py
+++ main.py
@@ -14,8 +14,13 @@ from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
     default="./qrcode.png",
     help='Output file path (defaults to "./qrcode.png")',
 )
+@click.option(
+    "--logo_path",
+    default=None,
+    help='Path to the logo to to embed (defaults to the Canonical one)',
+)
 @click.argument("url")
-def create(output_file, url):
+def create(output_file,logo_path, url):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_H,
@@ -24,14 +29,14 @@ def create(output_file, url):
     )
     qr.add_data(url)
     qr.make(fit=True)
-
+    logo=logo_path if logo_path else f"{os.environ.get("SNAP")}/assets/canonical.png"    
     qri = qr.make_image(
         image_factory=StyledPilImage,
         color_mask=SolidFillColorMask(
             back_color=(255, 255, 255), front_color=(233, 84, 32)
         ),
         module_drawer=RoundedModuleDrawer(),
-        embeded_image_path=f"{os.environ.get("SNAP")}/assets/canonical.png",
+        embeded_image_path=logo,
     )
 
     qri.save(output_file)
